a5d004a06c083af2fd917c727dbe29403e6c71ee
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2017-05-28 16:08:09 +0200

    added mapper

---
+++ src/main/java/com/hatorach/tcrs/Mapper.java
@@ -0,0 +1,11 @@
+package com.hatorach.tcrs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by rainerh on 23.04.17.
+ */
+@Service
+public class Mapper extends ModelMapper {
+}

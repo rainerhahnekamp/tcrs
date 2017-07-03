38613515756ca8246a3fcd73340a9f6c316e346f
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2017-07-03 09:49:41 +0200

    added dev profile

--- backend/src/main/java/com/hatorach/tcrs/Main.java
+++ backend/src/main/java/com/hatorach/tcrs/Main.java
@@ -1,17 +1,18 @@
 package com.hatorach.tcrs;
 
-import org.springframework.boot.SpringApplication;
+import com.hatorach.tcrs.misc.IgnoreInTestCoverage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * Created by CHJTOM on 19.03.2016.
  */
 
 @SpringBootApplication
+@IgnoreInTestCoverage
 public class Main {
-
   public static void main(String[] args) {
-    SpringApplication.run(Main.class);
+    String profile = System.getenv().getOrDefault("spring.profiles.active", "dev");
+    new SpringApplicationBuilder(Main.class).profiles("dev").run();
   }
-
 }

1d29ffe800ab102c2afeda652a6567b9590c040d
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2017-07-18 21:36:19 +0200

    fixed unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,25 @@
-/* tslint:disable:no-unused-variable */
-
 import {async, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {MdMenuModule} from '@angular/material';
+import {UserService} from './services/user-service.service';
+import {RouterTestingModule} from '@angular/router/testing';
 
-describe('App: Tcrs', () => {
-  beforeEach(() => {
+describe('AppComponent', () => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    });
-  });
+      imports: [MdMenuModule, RouterTestingModule],
+      providers: [
+        {provide: UserService, useValue: {getObservable: () => ({subscribe: () => true})}}
+      ]
+    }).compileComponents();
+  }));
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
 });

4588079a88bcea411ecb1caeac097e282701756c
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2018-01-13 15:49:55 +0100

    swagger executables

--- create-endpoint.sh
+++ create-endpoint.sh
@@ -8,3 +8,11 @@ docker run --rm \
     -c /local/swagger-cli-config.json \
     -l spring \
     -o /local/modules/endpoint
+
+docker run --rm \
+  -u `id -u $USER` \
+  -v $PWD:/local \
+  swaggerapi/swagger-codegen-cli generate \
+    -i /local/swagger.yaml \
+    -l typescript-angular \
+    -o /local/ng-endpoints
